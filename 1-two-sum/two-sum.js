10a231e3c9f0f480b36195de893a2d80551d4843
Rizky Darma <43875418+rizkydrma@users.noreply.github.com>
2024-01-24 17:22:04 +0700

    Time: 153 ms (10.79%) | Memory: 49.6 MB (5.04%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,8 +4,6 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let tamp = []
-
     for(let i = 0; i < nums?.length; i++){
         for(let j = i + 1; j < nums?.length; j++){
             if(nums[i] + nums[j] == target){
