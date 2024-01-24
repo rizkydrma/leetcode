443d5749100bf478933d848efa979d88e48dbd4c
Rizky Darma <43875418+rizkydrma@users.noreply.github.com>
2024-01-24 17:30:21 +0700

    Time: 49 ms (95.03%) | Memory: 49.8 MB (5.04%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,11 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums?.length; i++){
-        for(let j = i + 1; j < nums?.length; j++){
-            if(nums[i] + nums[j] == target){
-                return [i,j];
-            }
+     // Create an object to store the indices of elements
+    let indices = {};
+
+    // Iterate through the array
+    for (let i = 0; i < nums.length; i++) {
+        // Calculate the complement needed to reach the target
+        let complement = target - nums[i];
+
+        // Check if the complement is already in the object
+        if (indices.hasOwnProperty(complement)) {
+            // If found, return the indices of the two numbers
+            return [indices[complement], i];
         }
+
+        // If not found, add the current number and its index to the object
+        indices[nums[i]] = i;
     }
+
+    // If no solution is found, return an empty array (this should not happen based on the problem constraints)
+    return [];
 };
