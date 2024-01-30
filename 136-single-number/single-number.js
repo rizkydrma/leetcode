1e66f525d2a988cf0daa123cb245245dc7e27abb
Rizky Darma <43875418+rizkydrma@users.noreply.github.com>
2024-01-30 19:35:01 +0700

    add commant for single-number.js

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -6,13 +6,15 @@ var singleNumber = function(nums) {
     const myMap = new Map();
     nums.forEach((num) => {
         if (myMap.has(num)) {
+            // every data appear more than one, for this challange "twice"
+            // delete the data from myMap for minimize the size of myMap
             myMap.delete(num);
         } else {
             myMap.set(num, 1);
         }
     });
-    const isVal = [...myMap][0][0]
-    // const isVal = [...myMap].find(([key, value]) => 1 === value)[0]
 
+    // just get the first data from myMap cause every data appear more than one must be deleted
+    const isVal = [...myMap][0][0]
     return isVal;
 };
