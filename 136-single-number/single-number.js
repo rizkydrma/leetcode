0d4beb741ffc284d0926feed4c314eeaa9ad3bf9
Rizky Darma <43875418+rizkydrma@users.noreply.github.com>
2024-01-30 19:29:09 +0700

    Time: 46 ms (97.47%) | Memory: 54.5 MB (5.07%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -6,14 +6,13 @@ var singleNumber = function(nums) {
     const myMap = new Map();
     nums.forEach((num) => {
         if (myMap.has(num)) {
-            const currentVal = myMap.get(num);
-            myMap.set(num, currentVal + 1);
+            myMap.delete(num);
         } else {
             myMap.set(num, 1);
         }
     });
-
-    const isVal = [...myMap].find(([key, value]) => 1 === value)[0]
+    const isVal = [...myMap][0][0]
+    // const isVal = [...myMap].find(([key, value]) => 1 === value)[0]
 
     return isVal;
 };
